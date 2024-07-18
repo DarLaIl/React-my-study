7df01ad07ca7226a998f0e5a25ec0286a09c7d56
DarLaIl <darlail.work.mail@gmail.com>
2024-07-18 22:52:49 +0300

    Замена if на switch

--- 28-reducer/reducer.js
+++ 28-reducer/reducer.js
@@ -1,16 +1,30 @@
 const initialState = []
 
 function reducer(state, action) {
-    if (action.type === 'ADD_NAME') {
-        return [...state, action.payload]
-    }
-    if (action.type === 'DELETE_NAME') {
-        return state.filter(personName => personName !== action.payload);
-    }
-    if (action.type === 'CLEAR_NAMES') {
+    switch (action.type) {
+      case 'ADD_NAME':
+        return [...state, action.payload];
+      
+      case 'DELETE_NAME':
+        return state.filter((personName) => personName !== action.payload);
+
+      case 'CLEAR_NAMES':
         return [];
+
+      default:
+        return state;
     }
-    return state
+    
+    //if (action.type === 'ADD_NAME') {
+    //    return [...state, action.payload]
+    //}
+    //if (action.type === 'DELETE_NAME') {
+    //    return state.filter(personName => personName !== action.payload);
+    //}
+    //if (action.type === 'CLEAR_NAMES') {
+    //    return [];
+    //}
+    //return state
 }
 
 let newState = reducer(initialState, { type: 'ADD_NAME', payload: 'DarLa' })
