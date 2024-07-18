42e1a6ac8eae354d31946d910912e55becb004a1
DarLaIl <darlail.work.mail@gmail.com>
2024-07-18 22:36:24 +0300

    Вызов функции reducer

--- 28-reducer/reducer.js
+++ 28-reducer/reducer.js
@@ -4,5 +4,24 @@ function reducer(state, action) {
     if (action.type === 'ADD_NAME') {
         return [...state, action.payload]
     }
+    if (action.type === 'DELETE_NAME') {
+        return state.filter(personName => personName !== action.payload);
+    }
     return state
 }
+
+let newState = reducer(initialState, { type: 'ADD_NAME', payload: 'DarLa' })
+
+console.log(newState);
+
+newState = reducer(newState, { type: 'ADD_NAME', payload: 'Sasha' });
+
+console.log(newState);
+
+newState = reducer(newState, { type: 'ADD_NAME', payload: 'Kate' });
+
+console.log(newState);
+
+newState = reducer(newState, { type: 'DELETE_NAME', payload: 'Kate' });
+
+console.log(newState);
