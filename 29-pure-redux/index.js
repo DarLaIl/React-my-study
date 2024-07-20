3b8e980d2c94fac0f7bb1987f337b7a14e97c87e
DarLaIl <darlail.work.mail@gmail.com>
2024-07-20 15:06:35 +0300

    Добавление actionCreators и actionTypes

--- 29-pure-redux/index.js
+++ 29-pure-redux/index.js
@@ -1,31 +1,26 @@
 import store from './redux/store.js';
-import getCurrentTime from './utils/getCurrentTime.js';
+import { addCurrentTime, clearTimes } from './redux/actionCreators.js';
 
 const addTimeBtn = document.querySelector('#addTime');
 
 addTimeBtn.addEventListener('click', () => {
-  store.dispatch({
-    type: 'ADD_CURRENT_TIME',
-    payload: getCurrentTime(),
-  });
+  store.dispatch(addCurrentTime());
 });
 
 const clearTimeBtn = document.querySelector('#clearTimes');
 
 clearTimeBtn.addEventListener('click', () => {
-  store.dispatch({
-    type: 'CLEAR_ALL_TIMES',
-  });
+  store.dispatch(clearTimes());
 });
 
-const timesList = document.querySelector('#timesList')
+const timesList = document.querySelector('#timesList');
 
 store.subscribe(() => {
-    timesList.innerHTML = ''
-    const times = store.getState()
-    times.forEach((time) => {
-        let li = document.createElement('li')
-        li.innerText = time
-        timesList.appendChild(li)
-    });
-})
+  timesList.innerHTML = '';
+  const times = store.getState();
+  times.forEach((time) => {
+    let li = document.createElement('li');
+    li.innerText = time;
+    timesList.appendChild(li);
+  });
+});
--- 29-pure-redux/redux/reducer.js
+++ 29-pure-redux/redux/reducer.js
@@ -1,13 +1,15 @@
-const initialState = []
+import * as actionTypes from './actionTypes.js';
+
+const initialState = [];
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-      case 'ADD_CURRENT_TIME':
-        return [...state, action.payload];
-      case 'CLEAR_ALL_TIMES':
-        return [];
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case actionTypes.ADD_CURRENT_TIME:
+      return [...state, action.payload];
+    case actionTypes.CLEAR_ALL_TIMES:
+      return [];
+    default:
+      return state;
+  }
 }
-export default reducer
+export default reducer;
