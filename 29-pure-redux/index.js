0fb6ca78d8985a20578ea4ad46818bdaf975afe3
DarLaIl <darlail.work.mail@gmail.com>
2024-07-20 15:10:47 +0300

    Проект с Redux без React

--- 29-pure-redux/index.js
+++ 29-pure-redux/index.js
@@ -3,15 +3,11 @@ import { addCurrentTime, clearTimes } from './redux/actionCreators.js';
 
 const addTimeBtn = document.querySelector('#addTime');
 
-addTimeBtn.addEventListener('click', () => {
-  store.dispatch(addCurrentTime());
-});
+addTimeBtn.addEventListener('click', () => store.dispatch(addCurrentTime()));
 
 const clearTimeBtn = document.querySelector('#clearTimes');
 
-clearTimeBtn.addEventListener('click', () => {
-  store.dispatch(clearTimes());
-});
+clearTimeBtn.addEventListener('click', () => store.dispatch(clearTimes()));
 
 const timesList = document.querySelector('#timesList');
 
